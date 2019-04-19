db6c948ab68f3c83ee616962be4c3e0625202f70
itmaster921 <dev.mobile.99@hotmail.com>
2019-04-19 10:08:32 +0800

    Adds photo upload and application pdf.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,17 +2,17 @@ class DashboardController < ApplicationController
   def index
   end
 
-  def instructions
-    if request.post?
-      @origin_step = RegistrationStep.find_by_name('Instructions')
-      @registration_form = current_user.registration_form
-      unless @registration_form.instructions_read
-        @registration_form.increment(:current_step)
-        @registration_form.update(instructions_read: true)
-      end
-      render :proceed2_next
-    end
-  end
+  # def instructions
+  #   if request.post?
+  #     @origin_step = RegistrationStep.find_by_name('Instructions')
+  #     @registration_form = current_user.registration_form
+  #     unless @registration_form.instructions_read
+  #       @registration_form.increment(:current_step)
+  #       @registration_form.update(instructions_read: true)
+  #     end
+  #     render :proceed2_next
+  #   end
+  # end
 
   def personal
   end
